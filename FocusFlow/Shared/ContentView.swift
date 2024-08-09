c39f12f9ae362081c0c8902a46c06af3b69a5103
Lluvia jing <noturlluvia@gmail.com>
2024-08-09 14:42:22 -0700

    Initial commit

---
+++ FocusFlow/Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Lluvia Jing on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+https://github.com/noturlluvia/planning-scheduling-tool.git
---
+++ FocusFlow/Shared/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  Shared
+//
+//  Created by Lluvia Jing on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
